0159c0be83c0b3a91d06b34db541e65854ad1f0b
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-03 20:06:37 -0600

    Initial Commit

---
+++ Shared/HW_2_Alternating_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2_Alternating_SeriesApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HW_2_Alternating_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
