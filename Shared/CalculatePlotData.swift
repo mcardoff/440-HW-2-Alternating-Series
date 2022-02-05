d739b5f3813fc4cb8973782ae68400a9e1e58299
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-04 20:15:20 -0600

    Plotting troubles

---
+++ Shared/CalculatePlotData.swift
@@ -0,0 +1,123 @@
+//
+//  CalculatePlotData.swift
+//  SwiftUICorePlotExample
+//
+//  Created by Jeff Terry on 12/22/20.
+//
+
+import Foundation
+import SwiftUI
+import CorePlot
+
+class CalculatePlotData: ObservableObject {
+    
+    var plotDataModel: PlotDataClass? = nil
+    var theText = ""
+    
+    
+    @MainActor func setThePlotParameters(color: String, xLabel: String, yLabel: String, title: String) {
+        //set the Plot Parameters
+        plotDataModel!.changingPlotParameters.yMax = 10.0
+        plotDataModel!.changingPlotParameters.yMin = -5.0
+        plotDataModel!.changingPlotParameters.xMax = 10.0
+        plotDataModel!.changingPlotParameters.xMin = -5.0
+        plotDataModel!.changingPlotParameters.xLabel = xLabel
+        plotDataModel!.changingPlotParameters.yLabel = yLabel
+        
+        if color == "Red"{
+            plotDataModel!.changingPlotParameters.lineColor = .red()
+        }
+        else{
+            
+            plotDataModel!.changingPlotParameters.lineColor = .blue()
+        }
+        plotDataModel!.changingPlotParameters.title = title
+        
+        plotDataModel!.zeroData()
+    }
+    
+    @MainActor func appendDataToPlot(plotData: [plotDataType]) {
+        plotDataModel!.appendData(dataPoint: plotData)
+    }
+    
+    func plotYEqualsX() async {
+        await plotFunc(fun: {(x:Double) -> Double in return x}, funcName: "y = x", color: "Blue")
+    }
+    
+    
+    func ploteToTheMinusX() async {
+        await plotFunc(fun: {(x:Double) -> (Double) in return exp(-x)}, funcName: "y = exp(-x)", color: "Red")
+        return
+    }
+    
+    func plotLogLogErr() async {
+        let calculator = Calculator()
+        //set the Plot Parameters
+        await setThePlotParameters(color: "Red",
+                    xLabel: "x", yLabel: "Log(Err)", title: "log log plot")
+        await resetCalculatedTextOnMainThread()
+        
+        var plotData :[plotDataType] =  []
+        for n in 1 ..< 1000 {
+            let s1Double = calculator.calculateFiniteSum(
+                function: calculator.sumOneTerm(n:), minimum: 0, maximum: 2*n)
+            let s3Double = calculator.calculateFiniteSum(
+                function: calculator.sumThreeTerm(n:), minimum: 0, maximum: n)
+            //create x values here
+            let x = Double(n)
+            let logx = log10(x)
+            //create y values here
+            let y = log10(calculator.calcErr(actual: s3Double, calculated: s1Double))
+            
+            let dataPoint: plotDataType = [.X: x, .Y: y]
+            plotData.append(contentsOf: [dataPoint])
+            theText += "x = \(logx), y = \(y)\n"
+        }
+        
+        await appendDataToPlot(plotData: plotData)
+        await updateCalculatedTextOnMainThread(theText: theText)
+    }
+    
+    func plotFunc(fun: ((Double) -> Double), funcName: String, color: String) async {
+        
+        //set the Plot Parameters
+        await setThePlotParameters(color: color, xLabel: "x", yLabel: funcName, title: funcName)
+        await resetCalculatedTextOnMainThread()
+        
+        var plotData :[plotDataType] =  []
+        for i in 0 ..< 60 {
+
+            //create x values here
+            let x = -2.0 + Double(i) * 0.2
+
+            //create y values here
+            let y = fun(x)
+            
+            let dataPoint: plotDataType = [.X: x, .Y: y]
+            plotData.append(contentsOf: [dataPoint])
+            theText += "x = \(x), y = \(y)\n"
+        }
+        
+        await appendDataToPlot(plotData: plotData)
+        await updateCalculatedTextOnMainThread(theText: theText)
+        
+        return
+    }
+    
+    
+    @MainActor func resetCalculatedTextOnMainThread() {
+        //Print Header
+        plotDataModel!.calculatedText = ""
+        
+    }
+    
+    
+    @MainActor func updateCalculatedTextOnMainThread(theText: String) {
+        //Print Header
+        plotDataModel!.calculatedText += theText
+    }
+    
+}
+
+
+
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,7 +12,9 @@ typealias plotDataType = [CPTScatterPlotField : Double]
 
 struct ContentView: View {
     
+    @EnvironmentObject var plotData :PlotClass
     @ObservedObject private var calculator = Calculator()
+    @ObservedObject private var plotCalculator = CalculatePlotData()
     @State var maxTerms: String = "10"
     @State var s1sum: String = ""
     @State var s2sum: String = ""
@@ -22,66 +24,83 @@ struct ContentView: View {
     @State var s3err: String = "Not Defined"
     
     var body: some View {
-        HStack(alignment: .center) {
-            Text("N:")
-                .font(.callout)
-                .bold()
-            TextField("Enter Max Terms", text: $maxTerms, onCommit: self.calculateSums)
-                .frame(width: 100)
+        VStack {
+            CorePlot(dataForPlot: $plotData.plotArray[0].plotData, changingPlotParameters: $plotData.plotArray[0].changingPlotParameters)
+                .setPlotPadding(left: 10)
+                .setPlotPadding(right: 10)
+                .setPlotPadding(top: 10)
+                .setPlotPadding(bottom: 10)
+                .padding()
             
-        }.padding()
-        
-        HStack {
-            // Boxes that contain series values
-            VStack {
-                HStack {
-                    Text("S(1):")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S1", text: $s1sum).frame(width: 100)
-                }.padding()
+            Divider()
+            /*
+            HStack(alignment: .center) {
+                Text("N:")
+                    .font(.callout)
+                    .bold()
+                TextField("Enter Max Terms", text: $maxTerms, onCommit: self.calculateSums)
+                    .frame(width: 100)
                 
-                HStack {
-                    Text("S(2):")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S2", text: $s2sum).frame(width: 100)
-                }.padding()
-                
-                HStack {
-                    Text("S(3):")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S3", text: $s3sum).frame(width: 100)
-                }.padding()
-            }
-            // Errors wrt to S3
-            VStack {
-                HStack {
-                    Text("S(1) Err:")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S1", text: $s1err).frame(width: 100)
-                }.padding()
-                
-                HStack {
-                    Text("S(2) Err:")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S2", text: $s2err).frame(width: 100)
-                }.padding()
-                
-                HStack {
-                    Text("S(3) Err:")
-                        .font(.callout)
-                        .bold().frame(width: 50)
-                    TextField("S3", text: $s3err).frame(width: 100)
-                }.padding()
-            }
+            }.padding()
             
+            HStack {
+                // Boxes that contain series values
+                VStack {
+                    HStack {
+                        Text("S(1):")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S1", text: $s1sum).frame(width: 100)
+                    }.padding()
+                    
+                    HStack {
+                        Text("S(2):")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S2", text: $s2sum).frame(width: 100)
+                    }.padding()
+                    
+                    HStack {
+                        Text("S(3):")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S3", text: $s3sum).frame(width: 100)
+                    }.padding()
+                }
+                // Errors wrt to S3
+                VStack {
+                    HStack {
+                        Text("S(1) Err:")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S1", text: $s1err).frame(width: 100)
+                    }.padding()
+                    
+                    HStack {
+                        Text("S(2) Err:")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S2", text: $s2err).frame(width: 100)
+                    }.padding()
+                    
+                    HStack {
+                        Text("S(3) Err:")
+                            .font(.callout)
+                            .bold().frame(width: 50)
+                        TextField("S3", text: $s3err).frame(width: 100)
+                    }.padding()
+                }
+                
+            }
+            Button("Calculate Series", action: { self.calculateSums() } )
+                .padding()
+            */
+            HStack{
+                Button("exp(-x)", action: {
+                    Task.init{ await self.calculatePlots() } } )
+                    .padding()
+            }
         }
-        Button("Calculate Series", action: { self.calculateSums() } )
-            .padding()
     }
     
     func calculateSums() {
@@ -101,6 +120,25 @@ struct ContentView: View {
         s2err = String(format: "%0.9f", log10(calculator.calcErr(actual: s3Double, calculated: s2Double)))
         s3sum = String(format: "%0.9f", s3Double) // error not calculated for s3
         return
+    }
+    
+    @MainActor func setupPlotDataModel(selector: Int){
+        plotCalculator.plotDataModel = self.plotData.plotArray[0]
+    }
+    
+    func calculatePlots() async {
+        //pass the plotDataModel to the Calculator
+        setupPlotDataModel(selector: 0)
+        
+        let _ = await withTaskGroup(of:  Void.self) {
+            taskGroup in taskGroup.addTask {
+                //Calculate the new plotting data and place in the plotDataModel
+                await plotCalculator.plotLogLogErr()
+                
+                // This forces a SwiftUI update. Force a SwiftUI update.
+                await self.plotData.objectWillChange.send()
+            }
+        }
     }
     
 }
